9b6555695940277689e5afa2c3d64abf91bf5900
nwakpa mishak <meshaqrapha@gmail.com>
2022-08-09 03:08:51 -0700

    printf proj

---
+++ print_unsign_dec.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <studio.h>
+
+/**
+*print_number - prints integer
+*@arg: argument
+*Return: int
+*/
+int print_unsigned(va_list arg)
+
+{
+unsigned int m;
+int a = 1;
+int h,d,c, i = 0, l = 0;
+char b;
+char *k;
+k = &b;
+n = va_arg(arg, unsigned int);
+d = n + 0;
+if (n < 10 && n > 0)
+{
+b = n + '0';
+write(1, k, 1);
+l = l + 1;
+}
+else
+{
+while (d != 0)
+{
+i++;
+d = d / 10;
+}
+i = i -1;
+for (c = 0; c < i; c++)
+{
+a = a * 10;
+}
+b = (n / a) + '0';
+write (1, k, 1);
+l = l + 1;
+for (h = 0; h <= i - 1; h++)
+{
+a = a / 10;
+b = ((n / a) % 10) + '0';
+write (1, k, 1);
+l = l +1
+}
+}
+return (l);
+}
