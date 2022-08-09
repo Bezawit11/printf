592fb522dce7e779d0612eea78a86360c1f1be58
Bezawit11 <socbeza13@gmail.com>
2022-08-09 02:21:55 -0700

    my files

---
+++ ourfun.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int char_print(va_list arg)
+
+{
+char str[2] = {'c', '\0'};
+char a;
+a = va_arg(arg, int);
+*str = a;
+write(1, str, 1);
+return (1);
+}
+
+/**
+ *
+ *
+ */
+int string_print(va_list arg)
+
+{
+int count = 0;
+char *s;
+s = va_arg(arg, char*);
+if (s == NULL)
+{
+s = "(null)";
+}
+while (*s != '\0')
+{
+write(1, s, 1);
+count = count + 1;
+s++;
+}
+return (count);
+}
+
+/**
+ *
+ *
+ */
+int unsigned_bi(va_list arg)
+
+{
+unsigned int b;
+b = va_arg(arg, unsigned int);
+return (rec(b));
+}
+
+/**
+ *
+ *
+ */
+int rec(int n)
+
+{
+int r;
+char a;
+char *s;
+s = &a;
+if (n == 1)
+r = 1;
+if (n > 0)
+{
+r = n % 2;
+n = rec(n / 2);
+a = r + '0';
+write(1, s, 1);
+}
+return (1);
+}
+
+/**
+ *
+ *
+ *
+ */
+/*int percent_print()
+
+{
+write(1, "%", 1);
+return (1);
+}*/
+
+/**
+ *
+ *
+ *
+ */
+
+/*int decimal_print(int d)
+
+{
+if (d < 0)
+write(1, "-", 1);
+}*/
--- print_int.c
+++ print_int.c
@@ -1,24 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-#include <studio.h>
+#include "main.h"
 
 /**
-*print_number - prints integer
-*@arg: argument
-*Return: int
-*/
-int print_unsigned(va_list arg)
+ *  *print_number - prints integer
+ *   *@n: input
+ *    *Return: void
+ *     */
+int print_decimal(va_list arg)
 
 {
-unsigned int m;
-int a = 1;
-int h,d,c, i = 0, l = 0;
+int n, a = 1;
+int h, d, c, i = 0, l = 0;
 char b;
 char *k;
-k = &b;
-n = va_arg(arg, unsigned int);
+n = va_arg(arg, int);
 d = n + 0;
+k = &b;
 if (n < 10 && n > 0)
 {
 b = n + '0';
@@ -27,25 +26,31 @@ l = l + 1;
 }
 else
 {
+if (n < 0)
+{
+write(1, "-", 1);
+n = n * -1;
+l = l + 1;
+}
 while (d != 0)
 {
 i++;
 d = d / 10;
 }
-i = i -1;
+i = i - 1;
 for (c = 0; c < i; c++)
 {
 a = a * 10;
 }
 b = (n / a) + '0';
-write (1, k, 1);
+write(1, k, 1);
 l = l + 1;
 for (h = 0; h <= i - 1; h++)
 {
 a = a / 10;
 b = ((n / a) % 10) + '0';
-write (1, k, 1);
-l = l +1
+write(1, k, 1);
+l = l + 1;
 }
 }
 return (l);
