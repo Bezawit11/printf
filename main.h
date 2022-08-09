090ab5233b863b6334bfebd877077d4fbf1360d0
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-09 01:54:17 -0700

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_FILE
 #define MAIN_FILE
-<<<<<<< HEAD
 
 int _printf(const char*format, ...);
 int char_print(va_list arg);
@@ -11,7 +10,5 @@ int print_unsigned(va_list arg);
 int unsigned_bi(va_list arg);
 int rec(int n);
 int (*get_fs_func(const char *s))(va_list arg);
-=======
 int _printf(const char *format, ...);
->>>>>>> origin/first_task
 #endif
