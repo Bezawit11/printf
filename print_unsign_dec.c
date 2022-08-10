c20617517427b322689966cca36a4918f791ee3b
Bezawit11 <socbeza13@gmail.com>
2022-08-10 13:19:09 -0700

    u

--- print_unsign_dec.c
+++ print_unsign_dec.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <limits.h>
 
 /**
 *print_number - prints integer
@@ -11,13 +12,18 @@
 int print_unsigned(va_list arg)
 
 {
-unsigned int n;
-int a = 1;
-int h, d, c, i = 0, l = 0;
+int n, a = 1, l = 0;
+unsigned int m;
+unsigned int h, d, c, i = 0;
 char b;
 char *k;
 k = &b;
-n = va_arg(arg, unsigned int);
+n = va_arg(arg, int);
+if (n == 0)
+{
+write(1, "0", 1);
+return (1);
+}
 d = n + 0;
 if (n < 10 && n > 0)
 {
@@ -27,6 +33,11 @@ l = l + 1;
 }
 else
 {
+if (n < 0)
+{
+m = UINT_MAX + n + 1;
+d = m;
+}
 while (d != 0)
 {
 i++;
@@ -37,13 +48,13 @@ for (c = 0; c < i; c++)
 {
 a = a * 10;
 }
-b = (n / a) +'0';
+b = (m / a) +'0';
 write(1, k, 1);
 l = l + 1;
 for (h = 0; h <= i - 1; h++)
 {
 a = a / 10;
-b = ((n / a) % 10) + '0';
+b = ((m / a) % 10) + '0';
 write(1, k, 1);
 l = l + 1;
 }
