3da0dee68328aea00c9101956cd1ffb997935d7e
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-10 06:58:24 -0700

    Update main.h

--- main.h
+++ main.h
@@ -17,6 +17,6 @@ int rot13(va_list arg);
 int reverse_array(va_list arg);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int length_bi(unsigned int b, unsigned int base);
-int non_specifier(va_lisr arg);
+int non_specifier();
 int (*get_fs_func(const char *s))(va_list arg);
 #endif
