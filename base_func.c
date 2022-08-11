f8c8f5a38cdb13ec0355116fd00e0754afbc7b00
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-08-11 06:45:34 -0700

    Update base_func.c

--- base_func.c
+++ base_func.c
@@ -30,6 +30,28 @@ return (length_base(n, 16));
  *
  *
  */
+int hexi_cap(va_list arg)
+
+{
+unsigned int n, t = 1;
+char s;
+char *k;
+k = &s;
+n = va_arg(arg, int);
+if (n == 0)
+{
+s = n + '0';
+write(1, k, 1);
+return (1);
+}
+invert_hex_cap(n ,16, t);
+return (length_base(n, 16));
+}
+
+/**
+ *
+ *
+ */
 int octal(va_list arg)
 
 {
@@ -102,6 +124,38 @@ write(1, s, 1);
 }
 return (1);
 }
+
+/**
+ *
+ *
+ */
+int invert_hex_cap(unsigned int n, unsigned int b, unsigned int t)
+
+{
+unsigned int r;
+char a;
+char *s;
+s = &a;
+if (n == 0)
+r = t;
+if (n > 0)
+{
+r = n % b;
+n = invert_hex_cap(n / b, b, n % b);
+if (r > 9 && r < 16)
+{
+a = r + '7';
+write(1, s, 1);
+}
+else
+{
+a = r + '0';
+write(1, s, 1);
+}
+}
+return (1);
+}
+
 /**
  *
  *
